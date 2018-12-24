9fcac3a39fa48c9c4245ccf8de3b2eef4ca49df9
ThGz <thgzlt@gmail.com>
2018-12-24 15:16:28 +0200

    commit 1

---
+++ src/app/users.service.ts
@@ -0,0 +1,40 @@
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import { map } from 'rxjs/operators';
+import {pipe} from "rxjs/internal-compatibility";
+
+
+
+@Injectable()
+export class UsersService {
+
+  size = 8;
+
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get('https://randomuser.me/api/?inc=gender,name,picture,location&results='+this.size+'&nat=gb')
+
+      .pipe(map(response => response.results))
+      .pipe(map(users=>{
+        return users.map(u=> {
+          return {
+            name: u.name.first + '' + u.name.last,
+            image: u.picture.large,
+            geo: u.location.city + '' + u.location.state + '' + u.location.street
+          }
+        })
+      }))
+}
+setSize(size){
+  this.size = size;
+}
+  users = [
+    {name: "WFM 1"},
+    {name: "WFM 2"},
+    {name: "WFM 3"},
+    {name: "WFM 4"},
+    {name: "WFM 5"},
+    {name: "WFM 6"}
+  ]
+}
